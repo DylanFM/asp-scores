439650b91c426f642ff08ed25dad7a1ed605626b
DylanFM <dylan.fm@gmail.com>
2009-04-18 17:18:11 +1000

    Extracts top two wave part of wave scores page.

--- heat-scores-scraper.rb
+++ heat-scores-scraper.rb
@@ -11,16 +11,31 @@ def doc
 end
 
 # Return the info from the top two waves section at the beginning of the page
+def get_top_two_waves_data(page)
+  page.css('body > center > table > tr > td > center > table td[align="center"] > table tr').collect do |competitor|
+    competitor.css('td.BodyL').collect { |cell| cell.content.strip }
+  end
+end
+
 def get_top_two_waves(page)
-  page.css('body > center > table > tr > td > center > table td[align="center"] > table tr').inject(Array.new) do |competitors, competitor|
-    competitors << competitor.children.css('td.BodyL').collect { |cell| cell.content.strip }
+  get_top_two_waves_data(page).collect do |competitor|
+    next unless competitor.join('|') =~ /^((\d)\w{2})\|([\w\s]+)(\((\w+)\s*\))\|(\d+\.[\w\d]{2})p,\s?(Win by|Needs|Comb.)\s(\d+\.[\w\d]{2})/i
+    match = Regexp.last_match
+    {
+      :place => match[2].to_i,
+      :name => match[3].strip,
+      :from => match[5],
+      :heat_total => match[6].to_f,
+      :diff => {
+        :status => match[7],
+        :amount => match[8].to_f
+      }
+    }
   end
 end
 
 begin
-  p get_top_two_waves(doc)
+  pp get_top_two_waves(doc)
 rescue Exception => e
   puts e
 end
-
-# >> [[], []]
