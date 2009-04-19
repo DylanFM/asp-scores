dabc222c1c693bfd91c8598cbb215926121d13b9
DylanFM <dylan.fm@gmail.com>
2009-04-19 23:12:55 +1000

    New example file.'

--- example/rip_curl_pro.rb
+++ example/rip_curl_pro.rb
@@ -9,7 +9,7 @@ rcp = CompScraper::Competition.create(
     :name => "Rip Curl Pro Bells Beach 2009"
   )
 
-# First 3 rounds
+# First 3 rounds # !> `&' interpreted as argument prefix
 (1..3).collect do |n|
   # Make a new round
   round = rcp.rounds.build(:number => n)
@@ -17,4 +17,14 @@ rcp = CompScraper::Competition.create(
   round.save_heat_data
   # Save comp
   rcp.save
+end
+
+puts rcp.name
+rcp.rounds.all.each do |round|
+  puts "Round #{round.number}:"
+  round.heats.all.each do |heat|
+    winner = heat.competitors.first(:place => 1)
+    loser = heat.competitors.first(:place => 2)
+    puts "\tHeat #{heat.number} - #{winner.surfer.name} beat #{loser.surfer.name} who needed #{loser.diff_amount} points"
+  end
 end
